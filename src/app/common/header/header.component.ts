15dbb67695708290d7770589ee149d2744c56646
Robert Evans <robertevans@robertevansmb.com>
2018-11-26 16:20:48 -0500

    Start work on header component

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bwm-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  componentTitle = 'I am app component from component.ts';
+
+  clickHandler() {
+    alert('I AM CLICKED');
+  }
+}
