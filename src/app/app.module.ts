cd63c6a5c3bd9fc888cb0e8abdb62b0ca66822c4
Robert Evans <robertevans@robertevansmb.com>
2018-11-26 18:27:01 -0500

    Create rental component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RentalComponent
   ],
   imports: [
     BrowserModule
