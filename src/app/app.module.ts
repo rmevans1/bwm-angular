ef455efa256e4e545945d0fa27b42f85e3774200
Robert Evans <robertevans@robertevansmb.com>
2018-11-26 18:36:25 -0500

    Add temp module to demonstrate routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule, Router} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { RentalComponent } from './rental/rental.component';
+import { TempComponent } from './temp/temp.component';
+
+const routes: Routes = [
+  {path: '', component: RentalComponent },
+  {path: 'temp', component: TempComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RentalComponent
+    RentalComponent,
+    TempComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
