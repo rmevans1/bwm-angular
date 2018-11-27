ef327f01a0052a351fca81c01da48a6849880531
Robert Evans <robertevans@robertevansmb.com>
2018-11-27 13:06:39 -0500

    Install and configure dotenv to protect configuration variables

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
-const express = require('express');
+const express = require('express'),
+      mongoose = require('mongoose');
+
+require('dotenv').config({silent: true});
+
+//mongoose.connect()
 
 const app = express();
+
+console.log(process.env.DB_URL);
 
 app.get('/rentals', function(req, res){
   res.json({'success': true});
