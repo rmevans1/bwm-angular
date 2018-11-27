a7f6487e4642327922fc9305f2fdb9d2109d449c
Robert Evans <robertevans@robertevansmb.com>
2018-11-26 22:32:39 -0500

    Sample rental data

--- src/app/rental/rental-list/rental-list.component.ts
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -7,7 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RentalListComponent implements OnInit {
 
-  rentals: any[] = [1,2,3,4];
+  rentals: any[] = [{
+    id: 1,
+    title: 'Central Apartment',
+    city: 'New York',
+    street: 'Times Sqaure',
+    category: 'apartment',
+    image: 'http://via.placeholder.com/350x250',
+    bedrooms: 3,
+    description: 'Very nice apartment',
+    dailyRate: 34,
+    shared: false,
+    createdAt: '24/12/2017'
+  },
+    {
+      id: 2,
+      title: 'Central Apartment 2',
+      city: 'San Francisco',
+      street: 'Main street',
+      category: 'condo',
+      image: 'http://via.placeholder.com/350x250',
+      bedrooms: 2,
+      description: 'Very nice apartment',
+      dailyRate: 12,
+      shared: true,
+      createdAt: '24/12/2017'
+    },
+    {
+      id: 3,
+      title: 'Central Apartment 3',
+      city: 'Bratislava',
+      street: 'Hlavna',
+      category: 'condo',
+      image: 'http://via.placeholder.com/350x250',
+      bedrooms: 2,
+      description: 'Very nice apartment',
+      dailyRate: 334,
+      shared: true,
+      createdAt: '24/12/2017'
+    },
+    {
+      id: 4,
+      title: 'Central Apartment 4',
+      city: 'Berlin',
+      street: 'Haupt strasse',
+      category: 'house',
+      image: 'http://via.placeholder.com/350x250',
+      bedrooms: 9,
+      description: 'Very nice apartment',
+      dailyRate: 33,
+      shared: true,
+      createdAt: '24/12/2017'
+    }];
 
   constructor() { }
 
