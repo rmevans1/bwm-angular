751c496993e2a7c952dc9230b9aab30e511dd663
Robert Evans <robertevans@robertevansmb.com>
2018-11-26 23:18:38 -0500

    Fix home page to redirect to /rentals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import {Routes, RouterModule, Router} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
-import { RentalComponent } from './rental/rental.component';
 import { TempComponent } from './temp/temp.component';
 
 import { RentalModule} from './rental/rental.module';
 
 const routes: Routes = [
-  {path: '', component: RentalComponent },
+  {path: '', redirectTo: '/rentals', pathMatch: 'full' },
   {path: 'temp', component: TempComponent}
 ];
 
