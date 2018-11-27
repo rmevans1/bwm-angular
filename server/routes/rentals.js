541e40bba11a6716791f7ea6598d6b6f5284488b
Robert Evans <robertevans@robertevansmb.com>
2018-11-27 14:00:30 -0500

    Get all rentals

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,8 +1,11 @@
 const express = require('express'),
-      router = express.Router();
+      router = express.Router(),
+      Rental = require('../models/rental');
 
 router.get('', function(req, res) {
-  res.json({'ok': true});
+  Rental.find({}, function(err, foundRentals) {
+    res.json(foundRentals);
+  });
 })
 
 module.exports = router;
