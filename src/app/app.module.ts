e6c966219011940f4b91d771256bc823bbd135bf
Robert Evans <robertevans@robertevansmb.com>
2018-11-27 00:03:59 -0500

    Remove temp component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,17 @@ import {Routes, RouterModule, Router} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
-import { TempComponent } from './temp/temp.component';
 
 import { RentalModule} from './rental/rental.module';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/rentals', pathMatch: 'full' },
-  {path: 'temp', component: TempComponent}
+  {path: '', redirectTo: '/rentals', pathMatch: 'full' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    TempComponent,
+    HeaderComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
