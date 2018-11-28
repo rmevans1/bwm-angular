af175d30951a56626f73e89c8998c84387e92d96
Robert Evans <robertevans@robertevansmb.com>
2018-11-28 11:21:58 -0500

    Configure google maps api key

--- src/app/common/map/map.module.ts
+++ src/app/common/map/map.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
+import { MapComponent } from './map.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
+    MapComponent
+  ],
+  exports: [
+    MapComponent
   ],
   imports: [
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDpxebEnVe8ly-VN2x1QCifR2ENnaBV38s'
+    })
   ],
   providers: [],
   bootstrap: []
